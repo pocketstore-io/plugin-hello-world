3955df81e82e8d2e1957f0c5b115ea2d8a64fd48
jonathan-martz <github@jmartz.de>
2025-10-02 22:37:52 +0200

    add static version to template

--- pages/hello.vue
+++ pages/hello.vue
@@ -1,9 +1,9 @@
 <template>
   <section class="bg-white xl:mx-auto mx-3 px-3 py-3 max-w-6xl">
-    Hallo Welt {{ config.version}}
+    Hallo Welt {{ version}}
   </section>
 </template>
 
 <script setup lang="ts">
-import config from '../plugin.json'
+const version = ref('0.0.1.3');
 </script>
